416ccd0331bc7d52aa77d473970f061d1d991917
LuisCardoso <luiscardosoyusty@gmail.com>
2023-01-04 00:08:10 -0500

    cong dependencias y BD

---
+++ src/test/java/com/gestionEnfermedades/UniversidadBack/UniversidadBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionEnfermedades.UniversidadBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
